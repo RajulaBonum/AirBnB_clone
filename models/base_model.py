d6dc0dcd26e0176582974e91670d6424af2610bc
RajulaBonum <rajulabonum@email.com>
2023-12-10 04:16:00 -0800

    re-created an instance

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,26 @@ class BaseModel:
     Public instance attributes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initialize id, created_at, updated_at
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints class name, self.id and dictionarry representation"""
@@ -39,7 +52,7 @@ class BaseModel:
         of __dict__ of instance
         """
         mydict = self.__dict__.copy()
-        mydict['__class_'] = self.__class__.__name__
-        mydict['create_at'] = self.created_at.isoformat()
-        mydict['updated_at'] = self.updated_at.isoformat()
+        mydict["__class__"] = type(self).__name__
+        mydict["created_at"] = mydict["created_at"].isoformat()
+        mydict["updated_at"] = mydict["updated_at"].isoformat()
         return mydict
