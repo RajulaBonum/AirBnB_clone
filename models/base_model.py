a0b518a1f67eae1a05be6ab1a05c891316e25e1b
RajulaBonum <rajulabonum@gmail.com>
2023-12-11 23:00:27 -0800

    serialized and deserialized data

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""
-Class BaseModel that defines common attributes/methods for other classes
-Defines Public instances and methods
-"""
+"""This script is the base model"""
+
 import uuid
 from datetime import datetime
 from models import storage
 
 
 class BaseModel:
-    """
-    Public instance attributes
-    """
+
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
-        initialize id, created_at, updated_at
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == "created_at":
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -36,26 +35,22 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """prints class name, self.id and dictionarry representation"""
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, self.__dict__)
+        """Returns official string representation"""
 
-    """
-    Public instance methods
-    """
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates (update_at) with current time"""
-        self.update_at = datetime.now()
-        storage.save(self)
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of instance
-        """
-        mydict = self.__dict__.copy()
-        mydict["__class__"] = type(self).__name__
-        mydict["created_at"] = mydict["created_at"].isoformat()
-        mydict["updated_at"] = mydict["updated_at"].isoformat()
-        return mydict
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,44 +1,94 @@
 #!/usr/bin/python3
 """
-class FileStorage
-The class will serialize instances to JSON
-The class will also deserialze JSON to instances
+This is the storage engine
+Module for FileStorage class.
 """
+import datetime
 import json
+import os
 
 
 class FileStorage:
+
     """
-    Private class attributes
+    Private class attributes for storing and retriving data
     """
     __file_path = "file.json"
-    ___objects = {}
+    __objects = {}
 
-    """
-    Public instance methods
-    """
     def all(self):
-        """returns dictionary __objects"""
-        return self.__objects
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """sets in (__objects) obj with key (<obj class nem>.id)"""
-        key = "{}.{}".format(self.__class__.__name__, self.id)
-        self.__objects[key] = obj
+        """
+        sets in __objects the obj with key <obj class name>.id
+        obj is from BaseModel class
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes objects to JSON FILE(__file_path)"""
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            loaded = {k: v.to_dict() for k, in self.__objects.items()}
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            loaded = {
+                    k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(loaded, f)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+
+        classes = {"BaseModel": BaseModel}
+        return classes
+
     def reload(self):
-        """deserializes te JSON file to __objects"""
-        if not os.path.isfile(self.__file_path):
+        """
+        desirializes the stored objects from JSON to dictionary
+        """
+        if not os.path.isfile(FileStorage.__file_path):
             return
-        with open(self.__file_path, "r", encoding="utf-8") as f:
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             unloaded = json.load(f)
             unloaded = {k: self.classes()[v["__class__"]](**v)
-                        for key, value in unloaded.items()}
-            """Can or should this overwrite or insert?"""
-            self.__objects = unloaded
+                        for k, v in unloaded.items()}
+            """TO REVISE"""
+            FileStorage.__objects = unloaded
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
